0c0c5c63817dfc5ea2235093174c83b873005c52
mcagabe19 <egzozu.be.bas@gmail.com>
2025-01-23 14:17:59 +0000

    whoopsie

--- project/src/backend/sdl/SDLWindow.cpp
+++ project/src/backend/sdl/SDLWindow.cpp
@@ -28,7 +28,9 @@ namespace lime {
 	SDL_Cursor* SDLCursor::waitCursor = 0;
 	SDL_Cursor* SDLCursor::waitArrowCursor = 0;
 
-	#if !defined(IPHONE) && !defined(APPLETV) && !defined(ANDROID)
+	#if defined (IPHONE) || defined (APPLETV)
+	static bool displayModeSet = true;
+	#else
 	static bool displayModeSet = false;
 	#endif
 
@@ -47,13 +49,7 @@ namespace lime {
 
 		int sdlWindowFlags = 0;
 
-		#if defined(IPHONE) || defined(APPLETV) || defined(ANDROID)
-		if (flags & WINDOW_FLAG_FULLSCREEN) sdlWindowFlags |= SDL_WINDOW_FULLSCREEN;
-		#else
-		if (flags & WINDOW_FLAG_FULLSCREEN) sdlWindowFlags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
-		#endif
-
-		if (flags & WINDOW_FLAG_FULLSCREEN) sdlWindowFlags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
+		if (flags & WINDOW_FLAG_FULLSCREEN) sdlWindowFlags |= displayModeSet ? SDL_WINDOW_FULLSCREEN : SDL_WINDOW_FULLSCREEN_DESKTOP;
 		if (flags & WINDOW_FLAG_RESIZABLE) sdlWindowFlags |= SDL_WINDOW_RESIZABLE;
 		if (flags & WINDOW_FLAG_BORDERLESS) sdlWindowFlags |= SDL_WINDOW_BORDERLESS;
 		if (flags & WINDOW_FLAG_HIDDEN) sdlWindowFlags |= SDL_WINDOW_HIDDEN;
